9617d1de05c04d0dc2a7d518109bc016b96bc738
Frank Burkhart <frankburkhart@pop-os.localdomain>
2024-07-24 13:03:56 -0400

    Create module for getting game hrefs from main table

---
+++ games.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+driver = webdriver.Firefox()
+
+url = "https://www.cross-tables.com/annolistself.php"
+driver.get(url)
+driver.implicitly_wait(5)
+
+# Locate the table and find all the rows 
+table = driver.find_element(By.ID, "xtdatatable")
+rows = table.find_elements(By.TAG_NAME, 'tr')
+
+games = []
+for row in rows[1:]:
+    # Locate the anchor tag within the row
+    anchor = row.find_element(By.TAG_NAME, "a")
+    href = anchor.get_attribute('href') 
+    games.append(href)
+
+filename = "game_list.txt"
+with open(filename, 'w') as f:
+    for item in games:
+        f.write(f"{item}\n")
